7d5fcecb909eeddc0e96bc6aa0a9b9b285242b35
gilgonzalez <fragilgon@gmail.com>
2024-06-29 23:18:43 +0200

    mt

--- app/admin/products/[productId]/page.tsx
+++ app/admin/products/[productId]/page.tsx
@@ -32,8 +32,8 @@ const ProductPage = async ({params : {productId}} : Props) => {
 
   const product = await getProduct(productId)
   return (
-    <div className="flex mt-5 flex-col justify-center items-center h-full text-slate-800">
-      <div className="relative flex flex-col items-center rounded-[20px] w-fit bg-white bg-clip-border shadow-lg px-4 md:px-20  p-8">
+    <div className="flex mt-8 flex-col justify-center items-center h-full  text-slate-800">
+      <div className="relative flex flex-col pt-4 items-center rounded-[20px] w-fit bg-white bg-clip-border shadow-lg px-4 md:px-20  p-8">
         <div className="mt-2  w-full">
           <h1 className="px-2 text-xl font-bold text-slate-700 capitalize">
             #{product.id} {product.name}
